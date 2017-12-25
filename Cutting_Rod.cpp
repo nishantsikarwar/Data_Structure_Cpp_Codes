53977cb1eb793baec7b16c56560d210011ff11b7
nishantsikarwar <nishant.sikarwar007@gmail.com>
2017-12-25 19:11:23 +0530

    DP_Cutting_Rod_Solutions

--- Cutting_Rod.cpp
+++ Cutting_Rod.cpp
@@ -6,10 +6,10 @@
 using namespace std;
 #define endl "\n"
 #define vlong long long int 
-#define input 8
+#define input 5
 #define mod 1000000007
 
-int cut_rod ( int p[] , int n ,int length) {
+int cut_rod_memoized_up( int p[] , int n ,int length) {
 	
 	int look_up[length+1]={0}, temp;
 	//max_price[0]=0;
@@ -31,11 +31,45 @@ int cut_rod ( int p[] , int n ,int length) {
        return look_up[n];
 
 }
+
+int cut_rod_memoized_down( int p[] , int n ,int look_down[]) {
+	int temp;
+	
+	if( look_down [n] >= 0)
+		return look_down[n];
+
+	if(n == 0)
+		return 0;
+
+	else {
+		temp = INT_MIN;
+
+		FOR(i,1,n)
+          temp = max(temp,p[i-1]+ cut_rod_memoized_down(p,n-i,look_down));
+
+          look_down[n] = temp;
+
+	}
+	return temp;
+}
+
+int cut_rod_memoized_down( int p[], int n, int length ) {
+   
+   int look_down[length+1];
+
+      FOR(i,0,length)
+       look_down[i] = INT_MIN;
+
+       cut_rod_memoized_down(p,n,look_down);      
+  }
+
+
+
 int main() {
 
 	//speed 
 
 	int p[]={1,5,8,9,10,17,17,20,24,30};
-	cout<< cut_rod(p,input,sizeof(p)/sizeof(int))<<endl; 
+	cout<< cut_rod_memoized_down(p,input,sizeof(p)/sizeof(int))<<endl; 
          		// cout<<sizeof(p)<<endl;
 }
